93f0616f08966e2727ffc6cfb0d89f4c2aef5677
Guilherme Silva Guini <guil_guini@hotmail.com>
2017-08-08 21:42:05 -0300

    cat-clicker terminado

--- JavaScript/design-pattern/ud989-cat-clicker-ko-starter/js/app.js
+++ JavaScript/design-pattern/ud989-cat-clicker-ko-starter/js/app.js
@@ -1,22 +1,80 @@
-var ViewModel = function () {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://www.flickr.com/photos/');
+var initialCats = [
+    {
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+        nicknames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
+    },
+    {
+        clickCount: 0,
+        name: 'Tiger',
+        imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/xshamx/4154543904',
+        nicknames: ['Tiger']
+    },
+    {
+        clickCount: 0,
+        name: 'Scaredy',
+        imgSrc: 'img/22252709_010df3379e_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+        nicknames: ['Scaredy']
+    },
+    {
+        clickCount: 0,
+        name: 'Shadow',
+        imgSrc: 'img/1413379559_412a540d29_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/malfet/1413379559',
+        nicknames: ['Shadow']
+    }
+];
 
-    this.incrementerCount = function () {
-        this.clickCount(this.clickCount() + 1);
-    };
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nicknames = ko.observableArray(data.nicknames);
 
     this.levelCat = ko.computed(function() {
-        if(this.clickCount() < 11)
-            return "Newborn";
+        var title;
+        var clicks = this.clickCount();
+        if(clicks < 10) {
+            title = 'Newborn';
+        } else if (clicks < 50) {
+            title = 'Infant';
+        } else if (clicks < 100) {
+            title = 'Child';
+        } else if (clicks < 200) {
+            title = 'Teen';
+        } else if (clicks < 500) {
+            title = 'Adult';
+        } else {
+            title = 'Ninja';
+        }
 
-        if(this.clickCount() < 20)
-            return "Infant";
-
-        return "Old";
+        return this.name() + ' - ' + title;
     }, this);
+}
+
+var ViewModel = function () {
+    var self = this;
+
+    self.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem) {
+        self.catList.push( new Cat(catItem) );
+    });
+
+    self.currentCat = ko.observable( self.catList()[0] );
+
+    self.incrementerCount = function () {
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };
+
+    self.selectCat = function (clickedCat) {
+        self.currentCat(clickedCat);
+    };
 }
 
 ko.applyBindings(new ViewModel());
